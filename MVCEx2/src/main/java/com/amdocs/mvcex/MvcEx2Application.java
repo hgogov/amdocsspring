009d658279d747f6662e69dcaa4bd54b90593855
User <User@Lab-637595286285854457-1>
2021-06-23 02:45:47 +0530

    project init

---
+++ MVCEx2/src/main/java/com/amdocs/mvcex/MvcEx2Application.java
@@ -0,0 +1,13 @@
+package com.amdocs.mvcex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcEx2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcEx2Application.class, args);
+	}
+
+}
---
+++ MVCEx2/src/test/java/com/amdocs/mvcex/MvcEx2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdocs.mvcex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcEx2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
